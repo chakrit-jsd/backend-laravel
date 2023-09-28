fdc3f4984b48baf75f10964115526660f31cf230
chakrit-jsd <chakritabv@gmail.com>
2023-09-28 18:10:53 +0700

    set up project

---
+++ database/migrations/2023_09_28_110331_customers.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Customers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Scheme::create('customers', function (Blueprint $table) {
+            $table->increment('id')
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
